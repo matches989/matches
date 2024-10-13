df61eadef38f0816324013250a86ec9d8ea53485
matches <lkk424@gmail.com>
2024-10-13 16:32:22 +0800

    2024/10/13上課上傳

--- scrip.js
+++
@@ -1,4 +0,0 @@
-c='Hello';
-d='world';
-console.log('Hello Loki', a + b);
-console.log('Hello world');
---
+++ script.js
@@ -0,0 +1,4 @@
+c = 'Hello';
+d = 'world';
+console.log(c + ' ' + d);
+console.log(`${c} ${d}`);
